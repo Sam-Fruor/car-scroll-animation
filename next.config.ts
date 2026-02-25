92705344a8fd37fb265b1df1998b8d2d3923b74b
Shameer <sameer1645khan@gmail.com>
2026-02-26 01:52:16 +0530

    Config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig = {
     unoptimized: true, // GitHub Pages doesn't support Next.js server-side image optimization
   },
   // IMPORTANT: If your GitHub repo is named "car-scroll-animation", uncomment the line below:
-  // basePath: '/car-scroll-animation', 
+  basePath: '/car-scroll-animation', 
 };
 
 export default nextConfig;
